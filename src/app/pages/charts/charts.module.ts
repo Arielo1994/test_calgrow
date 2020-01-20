2cf145b34ca6110055500e90ee46bbb31979a244
Arielo1994 <mono_sensei@hotmail.com>
2020-01-20 10:32:28 -0300

    asdfasf

--- src/app/pages/charts/charts.module.ts
+++ src/app/pages/charts/charts.module.ts
@@ -6,7 +6,8 @@ import { ThemeModule } from '../../@theme/theme.module';
 
 import { ChartsRoutingModule, routedComponents } from './charts-routing.module';
 import { ChartjsMultipleXaxisComponent } from './chartjs/chartjs-multiple-xaxis.component';
-// import { PlantSelectorComponent } from './plant-selector/plant-selector.component';
+import { PlantSelectorComponent } from './plant-selector/plant-selector.component';
+import { ChartjsComponent } from './chartjs/chartjs.component';
 import { ComparativeChartComponent } from './comparative-chart/comparative-chart.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -18,7 +19,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const components = [
   ComparativeChartComponent,
-  ChartjsMultipleXaxisComponent
+  ChartjsMultipleXaxisComponent,
+  PlantSelectorComponent,
+  ChartjsComponent
 ];
 
 @NgModule({
